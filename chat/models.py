4ea1d9d77e0d434f7eaab59b301ef2ec5223d951
avneets2003 <avneets2003@gmail.com>
2023-11-24 02:25:06 +0530

    Initial commit

---
+++ chat/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Past(models.Model):
+	question = models.CharField(max_length=250)
+	answer =models.TextField(max_length=5000)
+
+	def __str__(self):
+		return self.question
---
+++ chat/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import cohere
+from .models import Past
+from django.core.paginator import Paginator
+
+def home(request):
+	if request.method == "POST":
+		question = request.POST['question']
+		past_responses = request.POST['past_responses']
+
+		co = cohere.Client('iqM4HhwTkXMPpx69FHopzPAYTk1l0mKEDQmvAOwe')
+
+		try:
+			response = co.chat(message=question, model='command').text
+
+			if "shamballa" in past_responses:
+				past_responses = response
+			else:
+				past_responses = f"{past_responses}<br/><br/>{response}"
+
+			record = Past(question=question, answer=response)
+			record.save()
+
+			return render(request, 'home.html', {"question": question, "response": response, "past_responses": past_responses})
+		except Exception as e:
+			return render(request, 'home.html', {"question":question, "response":e, "past_responses":past_responses})
+
+	return render(request, 'home.html', {})
+
+def past(request):
+	p = Paginator(Past.objects.all(), 5)
+	page = request.GET.get('page')
+	pages = p.get_page(page)
+	past = Past.objects.all()
+	nums = "a" * pages.paginator.num_pages
+	return render(request, 'past.html', {"past": past, "pages": pages, "nums": nums})
+
+def delete_past(request, Past_id):
+	past = Past.objects.get(pk=Past_id)
+	past.delete()
+	messages.success(request, ("Selected conversation has been deleted."))
+	return redirect('past')
